a50b49d6efc67bfcfe039bea1df6644283fde2f2
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-22 18:25:23 -0500

    created obj and funct() to connect to db, changed name of database

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE database employee_trackerDB;
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE database employee_tracker;
 
 USE employee_trackerDB;
 
--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const inquirer = require('inquirer');
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 const inqListInput = require('inquirer-list-input');
 
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: "8080",
+    user: "smbailey",
+    password: "BHogn_1953",
+    database: "employee_tracker"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    // need to call the prompts here nameOfPrompt();
+});
 
 promptUser();
