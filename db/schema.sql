6e0ffc58823cd42f1119b4c73efd9a279c9c684c
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-24 17:48:51 -0500

    corrected table cases which should be NOT NULL

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,25 @@ USE employee_tracker;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
+  title VARCHAR(30) NOT NULL,
 --  datatype decimal(m,d) precision and scale
 --  https://dev.mysql.com/doc/refman/8.0/en/precision-math-decimal-characteristics.html
-  salary DECIMAL(12.2) NULL,
-  department_id INT NULL,
+  salary DECIMAL(12.2) NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
    id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
   PRIMARY KEY (id)
 );
